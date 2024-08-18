3fc7095367c5d6cc73e4d9f9941140b2afdaf22a
Haps <thiamhapstou@gmail.com>
2024-08-18 11:47:51 +0000

    ajout du dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Components/Administration/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     //la route par défaut
+     {path:"", pathMatch:'full', redirectTo:'dashboard'},
+
+     //les routes pour les dashboards
+     {path:"dashboard", component:DashboardComponent},
+];
