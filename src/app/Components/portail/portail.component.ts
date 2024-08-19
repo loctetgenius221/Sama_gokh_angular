17d8247ffadfdef484f703e6bd8f5d4c5c7d1603
loctetgenius <sagnamoussa546@gmail.com>
2024-08-19 22:42:58 +0000

    Responsive desktop

--- src/app/Components/portail/portail.component.ts
+++ src/app/Components/portail/portail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-portail',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './portail.component.html',
   styleUrl: './portail.component.css'
 })
-export class PortailComponent {
+export class PortailComponent implements AfterViewInit {
+  private currentIndex: number = 0;
+  private items!: NodeListOf<HTMLElement>;
+  private totalItems!: number;
 
+  ngAfterViewInit() {
+    this.items = document.querySelectorAll('.carousel-item');
+    this.totalItems = this.items.length;
+    this.showItem(this.currentIndex);
+  }
+
+  showItem(index: number) {
+    const offset = -index * 100;
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    carousel.style.transform = `translateX(${offset}%)`;
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.totalItems - 1;
+    }
+    this.showItem(this.currentIndex);
+  }
+
+  next() {
+    if (this.currentIndex < this.totalItems - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+    this.showItem(this.currentIndex);
+  }
 }
