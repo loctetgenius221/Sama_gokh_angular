9646eae9d61c32ea7b2b3232d0cfb23ab2e2d396
loctetgenius <sagnamoussa546@gmail.com>
2024-08-19 09:30:59 +0000

    Intégration du portail d'entré v01

---
+++ src/app/Components/portail/portail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portail',
+  standalone: true,
+  imports: [],
+  templateUrl: './portail.component.html',
+  styleUrl: './portail.component.css'
+})
+export class PortailComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PortailComponent } from './Components/portail/portail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PortailComponent },
+];
