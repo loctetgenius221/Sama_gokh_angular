2cd1def420e230107448f35159903077bca51311
celineMend <celine.mendy@unchk.edu.sn>
2024-08-20 10:28:45 +0100

    detail des projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { HabitantHomeComponent } from './habitant/home/home.component';
 import { ProfileComponent } from './habitant/profile/profile.component';
 import { AddProjectComponent } from './habitant/add-project/add-project.component';
 import { EditProjectComponent } from './habitant/edit-project/edit-project.component';
+import { ViewProjectComponent } from './habitant/view-project/view-project.component';
+
 
 export const routes: Routes = [
   { path: "home", component: HabitantHomeComponent },
   { path: "profile", component: ProfileComponent },
   { path: 'add-project', component: AddProjectComponent },
   { path: 'edit-project/:id', component: EditProjectComponent },
- 
+  { path: "project/:id",component: ViewProjectComponent}
+
 ];
 
