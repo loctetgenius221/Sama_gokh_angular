5906a87e1fb887c5d9dd031dca3069eed8de1f4b
Haps <thiamhapstou@gmail.com>
2024-08-26 11:34:29 +0000

    correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 
 
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './Components/Administration/sidebar/sidebar.component';
-import { DashboardComponent } from './Components/Administration/dashboard/dashboard.component';
-import { SidebarMuniComponent } from './Components/Municipalite/sidebar-muni/sidebar-muni.component';
+// import { SidebarComponent } from './Components/Administration/sidebar/sidebar.component';
+// import { DashboardComponent } from './Components/Administration/dashboard/dashboard.component';
+// import { SidebarMuniComponent } from './Components/Municipalite/sidebar-muni/sidebar-muni.component';
 import { routes } from './app.routes';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HabitantHomeComponent } from './habitant/home/home.component';
 import { ProfileComponent } from './habitant/profile/profile.component';
+import { SidebarComponent } from './Components/Administration/sidebar/sidebar.component';
+import { SidebarMuniComponent } from './Components/Municipalite/sidebar-muni/sidebar-muni.component';
 
 
 @Component({
