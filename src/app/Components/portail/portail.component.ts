62f2d4d51be4c314350462aaafa4c278d58be94a
loctetgenius <sagnamoussa546@gmail.com>
2024-08-26 10:47:03 +0000

    vérification de la résolution du problème lier à github

--- src/app/Components/portail/portail.component.ts
+++ src/app/Components/portail/portail.component.ts
@@ -7,38 +7,6 @@ import { AfterViewInit, Component } from '@angular/core';
   templateUrl: './portail.component.html',
   styleUrl: './portail.component.css'
 })
-export class PortailComponent implements AfterViewInit {
-  private currentIndex: number = 0;
-  private items!: NodeListOf<HTMLElement>;
-  private totalItems!: number;
+export class PortailComponent {
 
-  ngAfterViewInit() {
-    this.items = document.querySelectorAll('.carousel-item');
-    this.totalItems = this.items.length;
-    this.showItem(this.currentIndex);
-  }
-
-  showItem(index: number) {
-    const offset = -index * 100;
-    const carousel = document.querySelector('.carousel') as HTMLElement;
-    carousel.style.transform = `translateX(${offset}%)`;
-  }
-
-  prev() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    } else {
-      this.currentIndex = this.totalItems - 1;
-    }
-    this.showItem(this.currentIndex);
-  }
-
-  next() {
-    if (this.currentIndex < this.totalItems - 1) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0;
-    }
-    this.showItem(this.currentIndex);
-  }
 }
