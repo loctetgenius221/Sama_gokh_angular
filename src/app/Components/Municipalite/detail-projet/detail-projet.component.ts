3e23973a328d8f164634401fe335db6ffe88f83c
Haps <thiamhapstou@gmail.com>
2024-08-27 10:13:54 +0000

    récuperation des votes dans la municipalites

--- src/app/Components/Municipalite/detail-projet/detail-projet.component.ts
+++ src/app/Components/Municipalite/detail-projet/detail-projet.component.ts
@@ -5,6 +5,7 @@ import { ProjetsService } from '../../../Services/projets.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommentairesService } from '../../../Services/commentaires.service';
 import localeFR from '@angular/common/locales/fr';
+import { VotesService } from '../../../Services/votes.service';
 
 registerLocaleData(localeFR, 'fr');
 
@@ -18,18 +19,21 @@ registerLocaleData(localeFR, 'fr');
 export class DetailProjetComponent implements OnInit {
   projet: any = {};  // Pour stocker les détails du projet
   commentaires: any[] = [];  // Pour stocker les commentaires du projet
+  votesPour: number = 0;
 
   constructor(
     private projetsService: ProjetsService,
     private route: ActivatedRoute,
-    private commentairesService: CommentairesService // Injecter le service des commentaires ici
+    private commentairesService: CommentairesService,
+    private votesService: VotesService
   ) {}
 
   ngOnInit(): void {
     const projetId = Number(this.route.snapshot.paramMap.get('id'));
     if (projetId) {
       this.getProjet(projetId);
-      this.getCommentaires(projetId); // Récupérer les commentaires associés au projet
+      this.getCommentaires(projetId); 
+      this.loadVotesPour(projetId);
     }
   }
 
@@ -49,12 +53,9 @@ export class DetailProjetComponent implements OnInit {
     );
   }
   
-  
-
   getCommentaires(projetId: number): void {
     this.commentairesService.getAllCommentaires().subscribe(
       (response: any) => {
-        console.log('Données reçues:', response); // Vérifiez la structure des données dans la console
         const commentairesPourProjet = response.data[projetId] || [];
         this.commentaires = commentairesPourProjet;
       },
@@ -63,8 +64,6 @@ export class DetailProjetComponent implements OnInit {
       }
     );
   }
-  
-  
 
   formatDate(dateString: string): string {
     const date = new Date(dateString);
@@ -72,5 +71,18 @@ export class DetailProjetComponent implements OnInit {
       return date.toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' });
     }
     return dateString;
+  }
+
+  loadVotesPour(projetId: number): void {
+    this.votesService.getAllVotes().subscribe(
+      (response: any) => {
+        const votesParProjet = response.data || [];
+        const projetVotes = votesParProjet.find((vote: any) => vote.projet_id === projetId);
+        this.votesPour = projetVotes ? projetVotes.total_votes : 0;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des votes pour:', error);
+      }
+    );
   }
 }
