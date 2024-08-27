73444c9b6029b4fdb1597648b1146ebbc0c34204
Haps <thiamhapstou@gmail.com>
2024-08-27 01:09:16 +0000

    affichage des projets avec leur images

--- src/app/habitant/home/home.component.ts
+++ src/app/habitant/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ProjetsService } from '../../Services/projets.service';
+import { ProjetsService } from '../../Services/projets.service'; 
+import { AuthService } from '../../Services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from '../../header/header.component';
 import { FooterComponent } from '../../footer/footer.component';
-
 
 @Component({
   selector: 'app-habitant-home',
@@ -16,22 +16,43 @@ import { FooterComponent } from '../../footer/footer.component';
 })
 export class HabitantHomeComponent implements OnInit {
   projets: any[] = [];
-  municipaliteId: number = 5; // Remplace par l'ID de la municipalité de l'habitant connecté
+  municipaliteId: number | undefined; 
 
-  constructor(private projetsService: ProjetsService) {}
+  constructor(
+    private projetsService: ProjetsService,
+    private authService: AuthService  
+  ) {}
 
   ngOnInit(): void {
-    this.loadProjets();
-  }
-
-  loadProjets(): void {
-    this.projetsService.getProjetsByMunicipalite(this.municipaliteId).subscribe(
-      response => {
-        this.projets = response.data;
+    this.authService.getUserDetails().subscribe(
+      (userDetails) => {
+        this.municipaliteId = userDetails.data.municipalite_id;
+        this.loadProjets();
       },
-      error => {
-        console.error('Erreur lors du chargement des projets:', error);
+      (error) => {
+        console.error('Erreur lors de la récupération des détails de l\'utilisateur:', error);
       }
     );
   }
+
+  loadProjets(): void {
+    if (this.municipaliteId) {
+      this.projetsService.getProjetsByMunicipalite(this.municipaliteId).subscribe(
+        response => {
+          const baseUrl = 'http://127.0.0.1:8000/storage/photos/'; 
+          this.projets = response.data.map(projet => ({
+            ...projet,
+            photo: projet.photo ? baseUrl + projet.photo : 'https://via.placeholder.com/300x200'
+          }));
+        },
+        error => {
+          console.error('Erreur lors du chargement des projets:', error);
+        }
+      );
+    } else {
+      console.error('ID de municipalité non disponible.');
+    }
+  }
+  
+  
 }
