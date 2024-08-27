55728b27b0046ce7120d066f7200e75de15dddc9
Haps <thiamhapstou@gmail.com>
2024-08-27 01:20:17 +0000

    réajustement

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  constructor(private router: Router) {}
 }
